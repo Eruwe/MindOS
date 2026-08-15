9b8903e4c0e0b2f8a0a2307aa347f5e5cc795175
YanaInu <yanainu@Noutbuk-YanaInu.local>
2026-08-15 11:58:18 +0300

    feat: autentification works, got my entry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: ['@nuxt/eslint', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false,
+  },
 });
